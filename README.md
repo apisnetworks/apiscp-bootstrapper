19523d87fc2272ff1cb2e1336fc0b4448bc2c6f6
Matt Saladna <msaladna@apisnetworks.com>
2019-02-05 16:06:34 -0600

    Provider benchmark process + reports Checkout by tag
    Signed-off-by: Matt Saladna <msaladna@apisnetworks.com>

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 [![Build Status](https://travis-ci.org/apisnetworks/apnscp-bootstrapper.svg?branch=master)](https://travis-ci.org/apisnetworks/apnscp-bootstrapper)
 
 # apnscp Bootstrap Utility
-Bootstrap Utility provides an automated installation process to setup [apnscp](https://apnscp.com) on a compatible CentOS/RHEL machine. Not to be confused with apnscp Bootstrapper, which is a specific component of apnscp's [playbooks](https://github.com/apisnetworks/apnscp-playbooks) and called by this utility once the minimum environment is setup.
+Bootstrap Utility ("bootstrap.sh") provides an automated installation process to setup [apnscp](https://apnscp.com) on a compatible CentOS/RHEL machine. Not to be confused with apnscp Bootstrapper, which is a specific component of apnscp's [playbooks](https://github.com/apisnetworks/apnscp-playbooks) and called by this utility once the minimum environment is setup.
 
 # Usage
 ## Trial mode
@@ -12,9 +12,10 @@ curl https://raw.githubusercontent.com/apisnetworks/apnscp-bootstrapper/master/b
 ```
 
 ## Registered licenses
+
 After registering on [my.apnscp.com](https://my.apnscp.com), use the token to automatically download the key.
 ```bash
-curl https://raw.githubusercontent.com/apisnetworks/apnscp-bootstrapper/master/bootstrap.sh | bash -s - <api token>
+curl https://raw.githubusercontent.com/apisnetworks/apnscp-bootstrapper/master/bootstrap.sh | bash -s - <api token> <optional common name>
 ```
 
 Alternatively, if you have the x509 key ("license.pem") available where <keyfile> is the absolute path.
@@ -22,29 +23,41 @@ Alternatively, if you have the x509 key ("license.pem") available where <keyfile
 curl https://raw.githubusercontent.com/apisnetworks/apnscp-bootstrapper/master/bootstrap.sh | bash -s - -k <keyfile>
 ```
 
+### Optional common name
+
+An optional common name may be specified after <api token>. This is a 64 character long unique identifier that describes your certificate and may be any printable character. Emojis will be transcoded to their 4-byte sequence. Sorry 🍆!
+
 ## Setting initial configuration
 
-`-s` may be used to set variables in [`apnscp-vars.yml`](https://github.com/apisnetworks/apnscp-playbooks/blob/master/apnscp-vars.yml), which is the initial configuration template for apnscp. Additionally any role defaults may be overridden through this usage (such as "[rspamd_enabled](https://github.com/apisnetworks/apnscp-playbooks/blob/master/roles/mail/rspamd/defaults/main.yml)"). Escape sequences and quote when necessary because variables are passed as-is to apnscp-vars.yml.
+apnscp may be customized via [apnscp.com's customizer](https://apnscp.com/#customize) or command-line. apnscp.com provides a selection of options that are adequate for most environments. Any variable may be overwritten for any play in the playbook. Common options are located in [`apnscp-vars.yml`](https://github.com/apisnetworks/apnscp-playbooks/blob/master/apnscp-vars.yml) and [`apnscp-internals.yml`](https://github.com/apisnetworks/apnscp-playbooks/blob/master/roles/common/vars/apnscp-internals.yml).
+
+### Command-line overrides
+
+`-s` may be used to set variables at installation in [`apnscp-vars.yml`](https://github.com/apisnetworks/apnscp-playbooks/blob/master/apnscp-vars.yml), which is the initial configuration template for apnscp. Additionally any role defaults may be overridden through this usage (such as "[rspamd_enabled](https://github.com/apisnetworks/apnscp-playbooks/blob/master/roles/mail/rspamd/defaults/main.yml)"). Escape sequences and quote when necessary because variables are passed as-is to apnscp-vars.yml.
 
 ```shell
 # Run bootstrap.sh from CLI setting apnscp_admin_email and ssl_hostnames in unattended installation
 bootstrap.sh -s apnscp_admin_email=matt@apisnetworks.com -s ssl_hostnames="['apnscp.com','apisnetworks.com']"
 ```
 
-These arguments may be passed as part of the one-liner too. Note usage of `-` after first `-s`.
+These arguments may be passed as part of the one-liner too. *Note usage of `-` after first `-s`.*
 
 ```bash
 curl https://raw.githubusercontent.com/apisnetworks/apnscp-bootstrapper/master/bootstrap.sh | bash -s - -s apnscp_admin_email=matt@apisnetworks.com -s ssl_hostnames="['apnscp.com','apisnetworks.com']"
 ```
 
+Standard bootstrap.sh options may also be applied after the options, for example
 
+```bash
+curl https://raw.githubusercontent.com/apisnetworks/apnscp-bootstrapper/master/bootstrap.sh | bash -s - -s apnscp_admin_email=matt@apisnetworks.com someactivationkey
+```
 
 # Benchmarking providers
 
-This utility can also be used to benchmark a provider since it runs unassisted from start to finish.
+bootstrap.sh can also be used to benchmark a provider since it runs unassisted from start to finish. For consistency commit [bc058b4](https://gitlab.com/apisnetworks/apnscp/tree/bc058b4807a29500485a239ff67c735f46405dc9) is referenced ("benchmark" tag). If `RELEASE` is omitted bootstrap.sh will use master, which may produce different results that the stats below.
 
 ```shell
-curl https://raw.githubusercontent.com/apisnetworks/apnscp-bootstrapper/master/bootstrap.sh | bash
+curl https://raw.githubusercontent.com/apisnetworks/apnscp-bootstrapper/master/bootstrap.sh | env RELEASE=benchmark bash 
 ```
 
 Check back in ~2 hours then run the following command:
@@ -53,32 +66,36 @@ Check back in ~2 hours then run the following command:
 IFS=$'\n' ; DATES=($((tail -n 1 /root/apnscp-bootstrapper.log | grep failed=0 ; grep -m 1 -P '^\d{4}-.*u=root' /root/apnscp-bootstrapper.log ) | awk '{print $1, $2}')) ; [[ ${#DATES[@]} -eq 2 ]] && python -c 'from datetime import datetime; import sys; format="%Y-%m-%d %H:%M:%S,%f";print datetime.strptime(sys.argv[1], format)-datetime.strptime(sys.argv[2], format)' "${DATES[0]}" "${DATES[1]}" || (echo -e "\n>>> Unable to verify Bootstrapper completed - is Ansible still running or did it fail? Last 10 lines follow" && tail -n 10 /root/apnscp-bootstrapper.log)
 ```
 
-A duration will appear or the last 10 lines of /root/apnscp-bootstrapper.log if it failed.
+A duration will appear or the last 10 lines of `/root/apnscp-bootstrapper.log` if it failed. This tests network/IO/CPU. A second test of backend performance once apnscp is setup gives the baseline performance between frontend/backend communication. This can be tested as follows,
+
+```bash
+cpcmd config_set apnscp.debug true  ; sleep 5 ; cpcmd test_backend_performance ; cpcmd config_set apnscp.debug false
+```
+
+debug mode will be temporarily enabled, which opens up access to the [test module](https://api.apnscp.com/class-Test_Module.html) API.
 
 ## Provider stats
 
 Bootstrapping should complete within 90 minutes on a single core VPS. Under 60 minutes is impressive. These are stats taken from [Bootstrapper](https://github.com/apisnetworks/apnscp-playbooks) initial runs as bundled with part of [apnscp](https://apisnetworks.com). Note that as with shared hosting, or any shared resource, performance is adversely affected by oversubscription and noisy neighbors. Newer hypervisors show better benchmark numbers whereas older hypervisors show lower performance figures.
 
-* Linode 2048, 1x CPU
-  * 1:17:23.97 (E5-2697, 4599 bogomips)
-  * 1:21:49.02 (E5-2697, 4599 bogomips)
-* Linode 4GB, 2x CPU (ATL data center)
-  * 1:19:57.583 (E5-2697, 4599 bogomips)
-* Linode 32GB, 8x CPU (NJ data center)
-  * 1:07:59.054 (E5-2680, 5600x8 bogomips)
-* DigitalOcean 2 GB, 1x CPU
-  * 1:39:49.07 (Gold 6140, 4589 bogomips)
-* DigitalOcean 2 GB, 1x High Throughput CPU
-  * 1:07:14.14 (Platinum 8168, 5387 bogomips)
-* DigitalOcean 2 GB, 2x CPU
-  * 1:47:06.25 (Gold 6140, 4589x2 bogomips)
-* Vultr 2GB, 1x CPU (NJ data center)
-  * 0:51:51.36 (Virtual CPU 82d93d4018dd, 5187 bogomips)
-    *yum.apnscp.com is presently on Vultr and whereas this affects a small segment of the test, performance was still impressive thus necessitating a retest in another location*
-* Vultr 2GB, 1x CPU (ATL data center retest)
-  * 1:26:46.265 (Virtual CPU 82d93d4018dd, 5187 bogomips)
-* Vultr 2GB, 1x CPU (CHI data center)
-  * 1:04:13.397 (Virtual CPU 82d93d4018dd, 5187 bogomips)
-* Vultr 32GB, 8x CPU (LA data center)
-  * 0:56:40.751 (Virtual CPU 82d93d4018dd, 5187x8 bogomips)
-  
+* AWS t3.small
+    * **Install:** 1:15:48, **Backend:** 7331 requests/second (2 GB, 2x vCPU, 2.5 GHz, P-8175 - 5000 bogomips)
+* AWS Lightsail
+    * **Install:** 2:31:08, **Backend:** 1475 requests/second (2 GB, 1x E5-2676 - 4789 bogomips)
+* DigitalOcean
+    * **Install:** 1:40:55, **Backend:** 6234 requests/second (2 GB, 1x E5-2650 - 4399 bogomips)
+* Hetzner
+    * **Install:** 1:05:52, **Backend:** 11397 requests/second (2 GB, 1x Skylake, IBRS - 4199 bogomips)
+    * **Install:** 1:26:13, **Backend:** 10776 requests/second (2 GB, 1x Skylake, IBRS - 4199 bogomips)
+* Linode
+    * **Install:** 1:12:16, **Backend:** 8199 requests/second (2 GB, 1x E5-2697 - 4599 bogomips)
+* OVH
+    * **Install:** 1:22:54, **Backend:** 7232 requests/second (2 GB, 1x "Haswell, no TSX" - 6185 bogomips)
+* Virmach
+    * **Install:** 3:27:12, **Backend:** 1302 requests/second (4 GB, 3x "QEMU Virtual CPU" - 4399 bogomips)
+* Vultr
+    * **Install:** 0:59:09, **Backend:** 11568 requests/second (2 GB, 1x "Virtual CPU" - 5187 bogomips)
+
+## Contributing
+
+Fork, benchmark, and submit a PR - even for benchmark results. This project is licensed under MIT. Have fun!
--- bootstrap.sh
+++ bootstrap.sh
@@ -10,6 +10,7 @@ LOG_PATH="${LOG_PATH:-/root/apnscp-bootstrapper.log}"
 # Feeling feisty and want to use screen or nohup
 WRAPPER=${WRAPPER:-""}
 APNSCP_YUM="http://yum.apnscp.com/apnscp-release-latest-7.noarch.rpm"
+RELEASE="${RELEASE:-""}"
 RHEL_EPEL_URL="https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm"
 APNSCP_VARS_FILE="${APNSCP_HOME}/resources/playbooks/apnscp-vars.yml"
 BOOTSTRAP_STUB="/root/resume_apnscp_setup.sh"
@@ -22,159 +23,165 @@ RED="\e[31m"
 GREEN="\e[32m"
 
 fatal() {
-  echo -e "${RED}${BOLD}ERR:${EMODE} $1"
-  echo -e "${RED}Installation failed${EMODE}"
-  popd > /dev/null 2>&1
-  exit 1
+	echo -e "${RED}${BOLD}ERR:${EMODE} $1"
+	echo -e "${RED}Installation failed${EMODE}"
+	popd > /dev/null 2>&1
+	exit 1
 }
 
 is_os() {
-  case ${1,,} in
-    "redhat") [[ -f /etc/redhat-release ]] && grep -q "Red Hat" /etc/redhat-release
-      return $?;;
-    "centos") [[ -f /etc/centos-release ]] && grep -q "CentOS" /etc/centos-release
-      return $?;;
-    *) fatal "Unknown OS $1"
-  esac
+	case ${1,,} in
+		"redhat") [[ -f /etc/redhat-release ]] && grep -q "Red Hat" /etc/redhat-release
+			return $?;;
+		"centos") [[ -f /etc/centos-release ]] && grep -q "CentOS" /etc/centos-release
+			return $?;;
+		*) fatal "Unknown OS $1"
+	esac
 }
 
 
 test -z "${DEBUG+x}" && test -f "$(dirname "$LICENSE_KEY")/config.ini" && fatal "apnscp already installed"
 
 install_yum_pkg() {
-  yum -y install "$@"
-  STATUS=$?
-  if [[ $STATUS -ne 0 ]] ; then
-    fatal "failed to install RPM $*"
-  fi
+	yum -y install "$@"
+	STATUS=$?
+	if [[ $STATUS -ne 0 ]] ; then
+		fatal "failed to install RPM $*"
+	fi
 }
 
 provisional_copy_config() {
-  ([[ ! -f /root/"$(basename "${APNSCP_VARS_FILE}")" ]] && cp "${APNSCP_VARS_FILE}" /root) || true
+	([[ ! -f /root/"$(basename "${APNSCP_VARS_FILE}")" ]] && cp "${APNSCP_VARS_FILE}" /root) || true
 }
 
 set_vars() {
 	test "${#EXTRA_VARS[@]}" -eq 0 && return
 	provisional_copy_config
-  VARS_FILE="/root/$(basename "${APNSCP_VARS_FILE}")"
+	VARS_FILE="/root/$(basename "${APNSCP_VARS_FILE}")"
 	for VAR in "${EXTRA_VARS[@]}"; do
 		( gawk '
-    {
-      idx = index($0,"=")
-      if (idx == 0) {
-        # Garbage input
-        exit
-      }
-      VAR[1] = substr($0, 0, idx-1);
-      VAR[2] = substr($0, idx+1);
-      regex = "^"VAR[1]":"
-      found = 0
-      while (( getline line < "'"$VARS_FILE"'") > 0 ) {
-        where = match(line, regex)
-        if (where != 0) {
-          print VAR[1]": "VAR[2]
-          found=1
-        } else {
-          print line;
-        }
-      }
-      if (!found) {
-        print VAR[1]": "VAR[2]
-      }
-    }' <<< "$VAR") > "${VARS_FILE}.$$" && mv "${VARS_FILE}.$$" "$VARS_FILE"
+		{
+			idx = index($0,"=")
+			if (idx == 0) {
+				# Garbage input
+				exit
+			}
+			VAR[1] = substr($0, 0, idx-1);
+			VAR[2] = substr($0, idx+1);
+			regex = "^"VAR[1]":"
+			found = 0
+			while (( getline line < "'"$VARS_FILE"'") > 0 ) {
+				where = match(line, regex)
+				if (where != 0) {
+					print VAR[1]": "VAR[2]
+					found=1
+				} else {
+					print line;
+				}
+			}
+			if (!found) {
+				print VAR[1]": "VAR[2]
+			}
+		}' <<< "$VAR") > "${VARS_FILE}.$$" && mv "${VARS_FILE}.$$" "$VARS_FILE"
 	done
 }
 
 prompt_edit() {
-  test -t 1 || return 1
-  while true; do
-    read -r -t 30 -p "Do you wish to edit initial configuration? Installation resumes in 30 seconds automatically. [y/n] " yn < /dev/tty
-    STATUS=$?
-    test $STATUS -ne 0 && return 1
-    case $yn in
-        [Yy]* ) return 0;;
-        [Nn]* ) return 1;;
-        * ) echo "Please answer yes or no.";;
-    esac
-  done
+	test -t 1 || return 1
+	while true; do
+		read -r -t 30 -p "Do you wish to edit initial configuration? Installation resumes in 30 seconds automatically. [y/n] " yn < /dev/tty
+		STATUS=$?
+		test $STATUS -ne 0 && return 1
+		case $yn in
+				[Yy]* ) return 0;;
+				[Nn]* ) return 1;;
+				* ) echo "Please answer yes or no.";;
+		esac
+	done
 }
 
 save_exit() {
-  echo -e "#!/bin/sh\n${BOOTSTRAP_COMMAND}" > "${BOOTSTRAP_STUB}"
-  chmod 755 "${BOOTSTRAP_STUB}"
-  provisional_copy_config
-  echo -e "${GREEN}SUCCESS! apnscp-vars.yml has been copied to /root${EMODE}"
-  echo ""
-  echo "Edit apnscp-vars.yml with nano or vi:"
-  echo -e "${BOLD}nano /root/apnscp-vars.yml${EMODE}"
-  echo ""
-  echo "Make changes then rerun the bootstrapper as:"
-  echo -e "${BOLD}sh ${BOOTSTRAP_STUB}${EMODE}"
-  exit 0
+	echo -e "#!/bin/sh\n${BOOTSTRAP_COMMAND}" > "${BOOTSTRAP_STUB}"
+	chmod 755 "${BOOTSTRAP_STUB}"
+	provisional_copy_config
+	echo -e "${GREEN}SUCCESS! apnscp-vars.yml has been copied to /root${EMODE}"
+	echo ""
+	echo "Edit apnscp-vars.yml with nano or vi:"
+	echo -e "${BOLD}nano /root/apnscp-vars.yml${EMODE}"
+	echo ""
+	echo "Make changes then rerun the bootstrapper as:"
+	echo -e "${BOLD}sh ${BOOTSTRAP_STUB}${EMODE}"
+	exit 0
 }
 
 activate_key() {
-  KEY=$1
-  CN=${2:+/$2}
-  [[ ${#KEY} -ge 10 ]] || fatal "Invalid activation key. Visit https://my.apnscp.com to purchase a key"
-  fetch_license /activate/"$KEY""$CN"
-  return 0
+	KEY=$1
+	CN=${2:+/$2}
+	[[ ${#KEY} -ge 10 ]] || fatal "Invalid activation key. Visit https://my.apnscp.com to purchase a key"
+	fetch_license /activate/"$KEY""$CN"
+	return 0
 }
 
 fetch_license() {
-  URL=${1:-""}
-  TMPKEY=$(mktemp license.XXXXXX)
-  install_yum_pkg curl
-  CODE=$(( $(curl -f -w '%{http_code}' -A "$KEY_UA" -o "$TMPKEY" "${LICENSE_URL}${URL}") ))
-  STATUS=$?
-  [[ $STATUS -ne 0 || $CODE -ge 300 || $CODE -lt 200 ]] && fatal "Failed to fetch activation key."
-  install_key "$TMPKEY"
-  return 0
+	URL=${1:-""}
+	TMPKEY=$(mktemp license.XXXXXX)
+	install_yum_pkg curl
+	CODE=$(( $(curl -f -w '%{http_code}' -A "$KEY_UA" -o "$TMPKEY" "${LICENSE_URL}${URL}") ))
+	STATUS=$?
+	[[ $STATUS -ne 0 || $CODE -ge 300 || $CODE -lt 200 ]] && fatal "Failed to fetch activation key."
+	install_key "$TMPKEY"
+	return 0
 }
 
 install_key() {
-  KEY=$1
-  [[ -f $KEY ]] || fatal "License key ${KEY} does not exist"
-  [[ -f $LICENSE_KEY ]] && mv -f ${LICENSE_KEY}{,.old}
-  mkdir -p "$(dirname "$LICENSE_KEY")"
-  cp "$KEY" "$LICENSE_KEY"
-  return 0
+	KEY=$1
+	[[ -f $KEY ]] || fatal "License key ${KEY} does not exist"
+	[[ -f $LICENSE_KEY ]] && mv -f ${LICENSE_KEY}{,.old}
+	mkdir -p "$(dirname "$LICENSE_KEY")"
+	cp "$KEY" "$LICENSE_KEY"
+	return 0
 }
 
 install() {
-  if is_os centos; then
-    install_yum_pkg epel-release
-  elif is_os redhat; then
-    rpm -Uhv "$RHEL_EPEL_URL" || true
-  fi
-  install_yum_pkg gawk ansible git yum-plugin-priorities yum-plugin-fastestmirror nano yum-utils screen
-  install_dev
-  install_apnscp_rpm
-  echo "Switching to stage 2 bootstrapper..."
-  echo ""
-  sleep 1
-  set_vars
-  prompt_edit && save_exit
-  pushd $APNSCP_HOME/resources/playbooks
-  trap 'fatal "Stage 2 bootstrap failed\nRun '\''$BOOTSTRAP_COMMAND'\'' to resume"' EXIT
-  eval "$BOOTSTRAP_COMMAND"
-  trap - EXIT
+	if is_os centos; then
+		install_yum_pkg epel-release
+	elif is_os redhat; then
+		rpm -Uhv "$RHEL_EPEL_URL" || true
+	fi
+	install_yum_pkg gawk ansible git yum-plugin-priorities yum-plugin-fastestmirror nano yum-utils screen
+	install_dev
+	install_apnscp_rpm
+	echo "Switching to stage 2 bootstrapper..."
+	echo ""
+	sleep 1
+	set_vars
+	prompt_edit && save_exit
+	pushd $APNSCP_HOME/resources/playbooks
+	trap 'fatal "Stage 2 bootstrap failed\nRun '\''$BOOTSTRAP_COMMAND'\'' to resume"' EXIT
+	eval "$BOOTSTRAP_COMMAND"
+	trap - EXIT
 }
 
 install_apnscp_rpm() {
-  rpm -ihv --force "$APNSCP_YUM"
+	rpm -ihv --force "$APNSCP_YUM"
 }
 
 install_dev() {
-  pushd "$APNSCP_HOME"
-  git init
-  git remote add origin "$APNSCP_REPO"
-  git fetch --depth=1
-  git checkout -t origin/master
-  git submodule update --init --recursive
-  pushd $APNSCP_HOME/config
-  find . -type f -iname '*.dist' | while read -r file ; do cp "$file" "${file%.dist}" ; done
-  popd
+	pushd "$APNSCP_HOME"
+	if test "$RELEASE" == ""; then
+		git init
+		git remote add origin "$APNSCP_REPO"
+		git fetch --depth=1
+		git checkout -t origin/master
+	else
+		git clone --bare --depth=1 --branch "$RELEASE" "$APNSCP_REPO" .git
+		git config --unset core.bare
+		git reset --hard
+	fi
+	git submodule update --init --recursive
+	pushd $APNSCP_HOME/config
+	find . -type f -iname '*.dist' | while read -r file ; do cp "$file" "${file%.dist}" ; done
+	popd
 }
 
 #install_bootstrapper
@@ -182,8 +189,8 @@ install_dev() {
 MODE=""
 
 while getopts "hs:k:t" opt ; do
-  case $opt in
-  	"s")
+	case $opt in
+		"s")
 			if [[ ! $OPTARG =~ ^[a-z]{1,}[a-z_0-9]{1,}=.+$ ]] ; then
 				echo -e \
 						"Invalid argument ${OPTARG} - pass as key=val\n" \
@@ -198,26 +205,26 @@ while getopts "hs:k:t" opt ; do
 			EXTRA_VARS+=("$OPTARG")
 		;;
 
-    "h")
-      echo -e "${BOLD}Usage${EMODE}: $(basename "$0") [-s OPTION=VALUE...] [-k KEYFILE] | <ACTIVATION KEY>\n"
-      echo "Install apnscp release. Either a key file in PEM format required (license.pem)"
-      echo "or if a fresh license activated, the license key necessary"
-      exit 1
-    ;;
-    "k")
-      KEY=$OPTARG
-      [[ ${OPTARG:0:1} != "/" ]] && KEY=$PWD/$KEY
-      install_key "$(realpath "$OPTARG")"
-      MODE=install
-    ;;
-    "t")
-      # Same as trial
-      MODE="install"
-    ;;
-    \?)
-      fatal "Incomplete or unknown option \`$opt'"
-    ;;
-  esac
+		"h")
+			echo -e "${BOLD}Usage${EMODE}: $(basename "$0") [-s OPTION=VALUE...] [-k KEYFILE] | <ACTIVATION KEY>\n"
+			echo "Install apnscp release. Either a key file in PEM format required (license.pem)"
+			echo "or if a fresh license activated, the license key necessary"
+			exit 1
+		;;
+		"k")
+			KEY=$OPTARG
+			[[ ${OPTARG:0:1} != "/" ]] && KEY=$PWD/$KEY
+			install_key "$(realpath "$OPTARG")"
+			MODE=install
+		;;
+		"t")
+			# Same as trial
+			MODE="install"
+		;;
+		\?)
+			fatal "Incomplete or unknown option \`$opt'"
+		;;
+	esac
 done
 shift $((OPTIND-1))
 
